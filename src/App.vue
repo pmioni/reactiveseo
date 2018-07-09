220d94182ac0cd95fe95cce7b06d0bf4fc83ed96
pmioni <pmioni@hce.it>
2018-07-09 17:24:13 +0200

    Page complete, docs directory as build target.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <Content/>
   </div>
 </template>
 
 <script>
-import Main from './components/Main'
+import Content from './components/Content'
 
 export default {
   name: 'app',
   components: {
-    Main
+    Content
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="hello">
+    <p><em>{{ msg }}</em></p>
+    <h2>{{ text1 }}</h2>
+    <p>{{ text2 }}</p>
+    <p><em>{{ msg2 }}</em></p>
+    <hr>
+    <div v-for="post of posts">
+      <h2>{{post.title}}</h2>
+      <p>{{post.data}}</p>
+      <p><em>(delay: {{ post.delay }}ms)</em></p>
+      <hr>
+    </div>
+    <div class="errors" v-if="errors.length > 0">
+      <h3>Javascript Errors, if any:</h3>
+      <ul>
+        <li v-for="error of errors">
+          {{ error }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Content',
+  data () {
+    return {
+      msg: 'Text below the above line is rendered by Vue.js reactively, but is rendered immediately, without REST calls. Here\'s a description of the novel:',
+      msg2: 'Text below the following line is rendered by Vue.js reactively, and comes from a REST service. Each character bio comes from a separate web service call, and each is delayed by a number of milliseconds. The delay of each REST call is detailed after  each text.',
+      text1: 'Infinite Jest',
+      text2: 'Infinite Jest (1996) is a novel written by David Foster Wallace. The lengthy and complex work takes place in a semi-parodic future version of North America. The novel touches on the topics of tennis; substance addiction and recovery programs; depression; child abuse; family relationships; advertising and popular entertainment; film theory; and Quebec separatism.',
+      errors: [],
+      services: [
+        {
+          name: 'James',
+          url: '/james',
+          delay: 0
+        },
+        {
+          name: 'Avril',
+          url: '/avril',
+          delay: 300
+        },
+        {
+          name: 'Hal',
+          url: '/hal',
+          delay: 600
+        },
+        {
+          name: 'Mario',
+          url: '/mario',
+          delay: 900
+        },
+        {
+          name: 'Orin',
+          url: '/orin',
+          delay: 1200
+        },
+        {
+          name: 'Pemulis',
+          url: '/pemulis',
+          delay: 1500
+        },
+        {
+          name: 'Stice',
+          url: '/stice',
+          delay: 1800
+        },
+        {
+          name: 'Wayne',
+          url: '/wayne',
+          delay: 2100
+        },
+        {
+          name: 'Gately',
+          url: '/gately',
+          delay: 2400
+        },
+        {
+          name: 'Joelle',
+          url: '/joelle',
+          delay: 2700
+        }
+      ],
+      posts: []
+    }
+  },
+  created () {
+    for (let service in this.services) {
+      console.log(service)
+      axios.get('http://hcetest.getsandbox.com' + this.services[service].url)
+
+      .then(response => {
+        // JSON responses are automatically parsed.
+        console.log(response.data)
+          this.posts.push(
+            {
+              title: this.services[service].name,
+              delay: this.services[service].delay,
+              data: response.data.status
+            }
+          )
+        console.log(this.posts)
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+    }
+
+    // async / await version (created() becomes async created())
+    //
+    // try {
+    //   const response = await axios.get(`http://jsonplaceholder.typicode.com/posts`)
+    //   this.posts = response.data
+    // } catch (e) {
+    //   this.errors.push(e)
+    // }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
